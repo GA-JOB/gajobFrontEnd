1130b59e1743595208fff4c346a6285744ebda3d
suyeonii99 <shinsuyeon0424@gamil.com>
2022-03-25 15:48:07 +0900

    [#7] slick slider 2개로 분리

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,9 +1,29 @@
-import { AlertDismissible } from "components/alert";
+import { TextField, InputAdornment } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import styled from "styled-components";
 import "bootstrap/dist/css/bootstrap.min.css";
 export const Search = () => {
   return (
     <>
-      <span>
+      <SearchWrapper>
+        <SearchField
+          label="Search"
+          variant="standard"
+          type="text"
+          // value={keyword}
+          // onChange={onChangeKeyword}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+          placeholder="Search"
+        />
+      </SearchWrapper>
+
+      {/* <span>
         <button type="button" className="btn btn-danger">
           danger
         </button>
@@ -14,7 +34,16 @@ export const Search = () => {
           success
         </button>
         <AlertDismissible />
-      </span>
+      </span> */}
     </>
   );
 };
+
+const SearchWrapper = styled.div`
+  margin-top: 2vw;
+`;
+const SearchField = styled(TextField)`
+  width: 35vw;
+  height: 3vw;
+  padding: 1vw;
+`;
