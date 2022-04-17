e55876438563095782389ea9d59331729097bf9d
JIYEONGSTAR <jiyeongstar@gmail.com>
2022-04-18 01:51:37 +0900

    [#45] 채용공고 페이지 수정

--- src/pages/jobPosting/JobPostingList.tsx
+++ src/pages/jobPosting/JobPostingList.tsx
@@ -5,6 +5,7 @@ import { IJobPostingCrawling } from "types";
 import { ColumnDefinition, ReactTabulatorOptions } from "react-tabulator";
 import "react-tabulator/lib/styles.css"; // default theme
 import "react-tabulator/css/bootstrap/tabulator_bootstrap.min.css"; // use Theme(s)
+import "styles/tabulator.scss";
 interface IJobPostingProps {
   data: IJobPostingCrawling[];
 }
@@ -13,7 +14,6 @@ export const JobPostingList = ({ data }: IJobPostingProps) => {
   const columns: ColumnDefinition[] | any = [
     { formatter: "rownum", hozAlign: "center", width: 40 },
     { title: "채용공고 제목", field: "title", width: 150 },
-
     { title: "회사명", field: "company" },
     { title: "급여타입", field: "salaryType" },
     { title: "급여", field: "salary" },
@@ -25,16 +25,13 @@ export const JobPostingList = ({ data }: IJobPostingProps) => {
     { title: "지역", field: "region" },
   ];
   const options: ReactTabulatorOptions = {
-    height: "100%",
-    movableRows: false,
-    movableColumns: true,
     pagination: true,
     paginationMode: "local",
     paginationSize: 10,
   };
 
   return (
-    <JobPostingWrapper>
+    <JobPostingWrapper className="JobPostingWrapper">
       <TabulatorStyle columns={columns} data={data} options={options} />
     </JobPostingWrapper>
   );
@@ -48,25 +45,4 @@ const TabulatorStyle = styled(ReactTabulator)`
   align-items: center;
   justify-content: center;
   text-align: center;
-
-  .tabulator-row table {
-    vertical-align: middle;
-    border-collapse: collapse;
-  }
-
-  .tabulator-row table img {
-    border: 2px solid #ccc;
-  }
-
-  .tabulator-row table tr td {
-    border: none;
-  }
-
-  .tabulator-row table tr td:first-of-type {
-    width: 60px;
-  }
-
-  .tabulator-row table tr td div {
-    padding: 5px;
-  }
 `;
